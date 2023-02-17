4cf1fac83f6fa4149c83b45359d105b6eaa5615c
Emile Hiot <emile@mac-mini-de-emile.home>
2023-02-17 14:33:24 +0100

    dark mode & mini clean

--- src/App.jsx
+++ src/App.jsx
@@ -3,37 +3,26 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import Profil from "./pages/Profil"
 import ShoppingCart from "./pages/ShoppingCard.jsx"
-import styled, { ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components';
 import {lightTheme, darkTheme, GlobalStyle} from "./style/themes";
 import {useSelector} from "react-redux";
 import { createGlobalStyle } from "styled-components"
 import { profilContext } from "./context/ProfilContext";
 
 
-const StyledApp = styled.div`
-    
-`;
-
 function App() {
 
     const themeDark = useSelector(state => state.darkMode.darkTheme);
-    const [theme, setTheme] = useState('light');
-
-    const themeToggler = () => {
-        themeDark === 'light' ? setTheme('dark') : setTheme('light');
-    };
 
     const [firstname, setFirstname] = useState("John");
     const [lastname, setLastname] = useState("Doe");
-    const [email, setEmail] = useState("test@mail.com");
+    const [email, setEmail] = useState("test@email.com");
     const [showProfil, setShowProfil] = useState(false);
 
   return (
       <>
           <profilContext.Provider value={{firstname, setFirstname, showProfil, setShowProfil, lastname, setLastname, email, setEmail}}>
           <ThemeProvider theme={themeDark === "light" ? lightTheme : darkTheme}>
-          <GlobalStyle />
-          <StyledApp>
           <GlobalStyle />
           <BrowserRouter>
               <Routes>
@@ -42,7 +31,6 @@ function App() {
                 <Route path="/card" element={<ShoppingCart />} />
               </Routes>
           </BrowserRouter>
-          </StyledApp>
           </ThemeProvider>
           </profilContext.Provider>
       </>
